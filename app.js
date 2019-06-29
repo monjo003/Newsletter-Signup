adc911298c70146e72764edd5dc353c1bd749de5
abenezermonjor <monjo003@morris.umn.edu>
2019-06-29 13:04:13 -0500

    setting up signup page and geting the data that is inside the input

--- app.js
+++ app.js
@@ -2,16 +2,22 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-// const request = require("request");
+const request = require("request");
 
-const app = express ();
+const app = express();
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
   res.sendfile(__dirname  + "/signup.html");
 });
 
+app.post("/", function(req,res) {
+   var firstname  = req.body.firstName;
+   var lastname  = req.body.lastName;
+   var email = req.body.email;
+});
 
 
 app.listen(3000, function() {
