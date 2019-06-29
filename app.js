e1b85dd28cd0d81ebdc1838cc66075c787d52a08
abenezermonjor <monjo003@morris.umn.edu>
2019-06-29 11:22:34 -0500

    add bootstrap signup templet and fixed minor typeos

--- app.js
+++ app.js
@@ -2,9 +2,17 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
+// const request = require("request");
 
 const app = express ();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendfile(__dirname  + "/signup.html");
+});
+
+
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
