0f38281dd7cdbfe054415b60b5b50e18c3c75600
abenezermonjor <monjo003@morris.umn.edu>
2019-06-29 20:52:57 -0500

    complete the signup page and store firstname, lastname, email on mailchimp account i created.

--- app.js
+++ app.js
@@ -17,9 +17,53 @@ app.post("/", function(req,res) {
    var firstname  = req.body.firstName;
    var lastname  = req.body.lastName;
    var email = req.body.email;
+
+   var data = {
+     members: [
+       {
+         email_address: email,
+         status: "subscribed",
+         merge_fields: {
+           FNAME: firstname,
+           LNAME: lastname
+         }
+       }
+     ]
+   };
+
+   var jsonData = JSON.stringify(data);
+
+
+   var options = {
+     url: "https://us3.api.mailchimp.com/3.0/lists/f2a294fa07",   // url i want to send a req to
+     method: "POST",   // specifing how we want our req to be processed
+     headers: {
+       "Authorization" : "monjo003 0d8e3c406253bca52edc2626de233365-us3 "  // any string and the api key for the authorization
+     },
+     body: jsonData    // content
+   };
+
+
+
+request(options, function(error, response, body){
+  if(error) {
+     console.log(error);
+  }
+  else{
+    console.log(response.statusCode);
+  }
+});
+
+
 });
 
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
 });
+
+
+// api key = 0d8e3c406253bca52edc2626de233365-us3
+
+//list id
+// f2a294fa07
